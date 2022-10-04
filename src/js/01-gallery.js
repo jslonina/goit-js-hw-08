4a3a55ca29fb68380815e714dd8d5ebb3e72a9f1
jslonina <99342408+jslonina@users.noreply.github.com>
2022-10-04 18:50:02 +0200

    all done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector(".gallery");
+const galleryItemsElement = galleryItems
+      .map(x => {
+            const image = `<a class="gallery__item" href="${x.original}" >
+    <img
+      class="gallery__image"
+      src="${x.preview}"
+      alt="${x.description}"
+      title="${x.description}"
+    />
+  </a>`;
+      return image;
+      })
+      .join(" ");
+
+gallery.insertAdjacentHTML("afterbegin", galleryItemsElement);
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
+
+
